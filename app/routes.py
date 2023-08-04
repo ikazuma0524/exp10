be740b100ddbc0a1a532305ac6a6cfe992ebfcdc
ikazuma0524 <111176304+ikazuma0524@users.noreply.github.com>
2023-08-04 23:23:31 +0900

    時間と荷重のプロットを表示するところまで

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 import pandas as pd
+import matplotlib
+from datetime import datetime, timedelta
+from matplotlib.dates import date2num
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -16,7 +20,18 @@ def analyze_file():
     file = request.files['file']
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(filepath)
-
-    # ファイル解析と画像生成のコード（前述のコードと同じ）
-
-    return render_template('result.html', image_path=image_path)
+    data_raw = pd.read_excel(filepath, header=13)
+    data = data_raw.iloc[:, :4]
+    data.columns = ['time','Analog1','Analog2','Analog3']
+    data['Load_N'] = data['Analog1']*200
+    data['Strain_percentage']=data['Analog2']/data['Analog2'].median()
+    data['Stroke_mm'] = data['Analog3']*6
+    def time_to_seconds(time):
+       return time.hour*3600 + time.minute*60 + time.second
+    data['time_seconds'] = data['time'].apply(time_to_seconds)
+    plt.plot(data['time_seconds'], data['Load_N'])
+    image_filename = 'result.png'
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_filename)
+    image_url = url_for('static', filename=f'images/{image_filename}')
+    plt.savefig(image_path)
+    return render_template('result.html', image_path=image_url)
