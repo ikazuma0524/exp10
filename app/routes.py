c889193e5411c89d1d7e9f9c4d5a43eb4c8cec40
ikazuma0524 <111176304+ikazuma0524@users.noreply.github.com>
2023-08-05 18:20:24 +0900

    second-sample

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,7 @@ def upload_file():
 def analyze_file():
     material = request.form['material']
     area = float(request.form['area'])
+    length = float(request.form['length'])
     file = request.files['file']
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(filepath)
@@ -24,13 +25,19 @@ def analyze_file():
     data = data_raw.iloc[:, :4]
     data.columns = ['time','Analog1','Analog2','Analog3']
     data['Load_N'] = data['Analog1']*200
-    data['Strain_percentage']=data['Analog2']/data['Analog2'].median()
+    Strain_percentage=data['Analog2'].median()
     data['Stroke_mm'] = data['Analog3']*6
+    data['Nominal Stress (MPa)'] = data['Load_N']/area
+    data['Nominal Strain(gage) (%)'] = data['Analog2']/Strain_percentage
+    data['Nominal Straain(Stroke)']= (data['Stroke_mm']*100)/length
+    data['True Stress (MPa)'] = data['Nominal Stress (MPa)']*(1+data['Nominal Strain(gage) (%)']/100)
     def time_to_seconds(time):
        return time.hour*3600 + time.minute*60 + time.second
     data['time_seconds'] = data['time'].apply(time_to_seconds)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Load (N)')
     plt.plot(data['time_seconds'], data['Load_N'])
-    image_filename = 'result.png'
+    image_filename = 'fig1.png'
     image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_filename)
     image_url = url_for('static', filename=f'images/{image_filename}')
     plt.savefig(image_path)
